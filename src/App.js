01a443c81c3094e49d562d5cdb57875e0a4c4ed2
bentaylor <ben10taylor10@hotmail.com>
2020-12-04 19:03:01 +0000

    framer motion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {motion} from "framer-motion"
+
+const boxVariants = {
+  hidden: {
+
+  },
+
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div transition={{duration: 0.7, type: "spring", stiffness: 120}} initial={{y: -500}} animate={{opacity: 1, rotate: 405, y: 0}}></motion.div>
+      <motion.div whileHover={{scale: 1.1, rotate: 45}} transition={{duration: 0.7, type: "spring", stiffness: 120}} initial={{y: -500}} animate={{opacity: 1, rotate: 90, y: 50}}></motion.div>
+      <motion.div transition={{duration: 0.7, type: "spring", stiffness: 120}} initial={{y: -500}} animate={{opacity: 1, rotate: 135, y: 150}}></motion.div>
     </div>
   );
 }
