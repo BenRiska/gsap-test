13a5f5a1d9523c492a3df42674541b86830e1c56
bentaylor <ben10taylor10@hotmail.com>
2020-12-06 17:46:27 +0000

    transition done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
 
 import './App.css';
-import {motion} from "framer-motion"
+import {motion, AnimatePresence} from "framer-motion"
+import {
+  Switch,
+  Route,
+  Link,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 
-const boxVariants = {
+const homeVariants = {
   hidden: {
-
+   
   },
+  visible: {
+    
+  },
+  exit: {
+    y: -700,
+  }
+}
 
+
+const aboutVariants = {
+  hidden: {
+    opacity: 0,
+    x: 0
+  },
+  visible: {
+    opacity: 1,
+    x: 50,
+    transition: {
+    }
+  },
+  exit: {
+    x: -0,
+    top: 0,
+    width: "100%",
+    height:"40vh",
+    borderRadius: "none",
+    transition: {
+      duration: 1
+    }
+  }
 }
 
 
 function App() {
+  const location = useLocation()
   return (
     <div className="App">
-      <motion.div transition={{duration: 0.7, type: "spring", stiffness: 120}} initial={{y: -500}} animate={{opacity: 1, rotate: 405, y: 0}}></motion.div>
-      <motion.div whileHover={{scale: 1.1, rotate: 45}} transition={{duration: 0.7, type: "spring", stiffness: 120}} initial={{y: -500}} animate={{opacity: 1, rotate: 90, y: 50}}></motion.div>
-      <motion.div transition={{duration: 0.7, type: "spring", stiffness: 120}} initial={{y: -500}} animate={{opacity: 1, rotate: 135, y: 150}}></motion.div>
+      <nav>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          
+        </ul>
+      </nav>
+      <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={location.key}>
+          <Route path="/about">
+            <Page2/>
+          </Route>
+          <Route path="/">
+            <Page1/>
+          </Route>
+      </Switch>
+      </AnimatePresence>
     </div>
   );
 }
 
 export default App;
+
+
+const Page1 = () => {
+  const history = useHistory()
+  return (<motion.div
+    className="after"
+           variants={homeVariants}
+           animate={"visible"}
+           initial={"hidden"}
+           exit={"exit"}
+           >
+            <h1>heyya</h1>
+          </motion.div>)
+}
+
+const Page2 = () => {
+  const history = useHistory()
+  return (
+          
+          <motion.p
+           className="expand"
+           variants={aboutVariants}
+           animate={"visible"}
+           initial={"hidden"}
+           exit={"exit"}
+          >
+            <h1>hiya</h1>
+          </motion.p>
+  )
+}
